084b28213ac145eb3286b3dcafbe1be3f3fc9c2a
LaSarsoJackson <LaSarsoJackson@users.noreply.github.com>
2022-04-05 18:55:07 -0400

    Added new core features
    ESRI leaflet, new geolocate button

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,61 +1,71 @@
-import { React, useState, useRef, useEffect } from "react";
-import { MapContainer, TileLayer, Popup, Marker, GeoJSON, LayersControl, LayerGroup, useMap } from "react-leaflet";
+import { React, useState, useRef } from "react";
+import { MapContainer, Popup, Marker, GeoJSON, LayersControl, LayerGroup } from "react-leaflet";
 //import MarkerClusterGroup from "react-leaflet-markercluster";
 import "./index.css";
 import geo_burials from "./data/Geo_Burials.json";
-//eventually you should move this to an async thing so that you cna hydrate the map with the data
-
+//eventually you should move this to an async thing so that you could hydrate the map with the data
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import Button from '@mui/material/Button';
 import ARC_Roads from "./data/ARC_Roads.json";
 import ARC_Boundary from "./data/ARC_Boundary.json";
 import ARC_Sections from "./data/ARC_Sections.json";
+import { BasemapLayer } from "react-esri-leaflet";
+import PinDropIcon from '@mui/icons-material/PinDrop';
 
+
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
 
 const columns = [
-  { field: 'OBJECTID', sortable: true, filter: true , hide: true  },
+  { field: 'OBJECTID', sortable: true, hide: true },
   {
-    field: 'First_Name', sortable: true, filter: true, checkboxSelection: true,
+    field: 'First_Name', sortable: true, checkboxSelection: true,
   },
   {
-    field: 'Last_Name', sortable: true, filter: true
+    field: 'Last_Name', sortable: true,
   },
   {
-    field: 'Section', sortable: true, filter: true
+    field: 'Birth', sortable: true,
   },
   {
-    field: 'Lot', sortable: true, filter: true
+    field: 'Death', sortable: true,
   },
   {
-    field: 'Pvt_Pub', sortable: true, filter: true, hide: true 
+    field: 'Section', sortable: true,
   },
   {
-    field: 'Tier', sortable: true, filter: true
+    field: 'Lot', sortable: true,
   },
   {
-    field: 'Grave', sortable: true, filter: true
+    field: 'Pvt_Pub', sortable: true, hide: true
   },
   {
-    field: 'Sec_Disp', sortable: true, filter: true, hide: true 
+    field: 'Tier', sortable: true,
   },
   {
-    field: 'ARC_GeoID', sortable: true, filter: true
+    field: 'Grave', sortable: true,
   },
   {
-    field: 'Birth', sortable: true, filter: true
+    field: 'Sec_Disp', sortable: true, hide: true
   },
   {
-    field: 'Death', sortable: true, filter: true
+    field: 'ARC_GeoID', sortable: true,
   },
   {
-    field: 'Geotype', sortable: true, filter: true, hide: true 
+    field: 'Geotype', sortable: true, hide: true
   },
   {
-    field: 'Coordinates', sortable: true, filter: true, hide: true 
+    field: 'Coordinates', sortable: true, hide: true
   },
 ];
+
+const defaultColDef = {
+  // set filtering on for all columns
+  filter: true,
+  floatingFilter: true,
+};
+
 let geo_burials_rows = [];
 for (let i = 0; i < geo_burials.features.length; i++) {
   let feature = geo_burials.features[i];
@@ -82,9 +92,8 @@ for (let i = 0; i < geo_burials.features.length; i++) {
 export default function Map() {
   const gridRef = useRef(null);
   const [subsetData, setSubsetData] = useState([]); //this is the data that is selected
-
-  const displaySubset = subsetData.map((data, index) => 
-  {
+  const {BaseLayer} = LayersControl;
+  const displaySubset = subsetData.map((data, index) => {
     return (
       <Marker key={index} position={[data.Coordinates[1], data.Coordinates[0]]}>
         <Popup>
@@ -102,48 +111,38 @@ export default function Map() {
     )
   })
 
-
-  
   const onButtonClick = e => {
     const selectedNodes = gridRef.current.api.getSelectedNodes()
     if (selectedNodes.length > 0) {
-      //alert('Please select only one row')
       setSubsetData(selectedNodes.map(node => node.data))
-      //console.log(subsetData); //this is the data that is selected
-      //const selectedData = selectedNodes.map(node => node.data)
-      //setSubsetData(selectedData)
     } else if (selectedNodes.length === 0) {
       alert('Please select a row')
-    } 
+    }
   };
 
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState('Find me');
 
-  //what did you see John lombardi? when did you see it?
-  //https://stackoverflow.com/questions/71121283/passing-data-to-leaflet-from-ag-grid-programmitically
-
-
-  //https://codesandbox.io/s/how-to-set-the-map-to-a-geolocation-on-map-load-with-react-leaflet-v3-uvkpz?file=/src/Maps.jsx
-  function LocationMarker() {
-    const [position, setPosition] = useState(null);
-
-    const map = useMap();
-
-    useEffect(() => {
-      map.locate().on("locationfound", function (e) {
-        setPosition(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
+  const onLocateMarker = () => {
+    if (!navigator.geolocation) {
+      setStatus('Geolocation is not supported by your browser');
+    } else {
+      setStatus('Locating...');
+      navigator.geolocation.getCurrentPosition((position) => {
+        setStatus('Find me');
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      }, () => {
+        setStatus('Unable to retrieve your location');
       });
-    }, [map]);
-
-    return position === null ? null : (
-      <Marker position={position}>
-        <Popup>You are here</Popup>
-      </Marker>
-    );
+    }
   }
 
-  return (
+  //https://codesandbox.io/s/how-to-set-the-map-to-a-geolocation-on-map-load-with-react-leaflet-v3-uvkpz?file=/src/Maps.jsx
 
+
+  return (
     <div>
       <MapContainer
         center={[42.704180, -73.731980]}
@@ -156,19 +155,40 @@ export default function Map() {
               Get selected burial:
             </Button>
           </div>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            maxZoom={25}
-          />
-          {/*<LocationMarker></LocationMarker>*/}
+          <div className="buttonBox2">
+            <Button onClick={onLocateMarker} className='locate-button' variant='contained' color='secondary' size='small' startIcon={<PinDropIcon/>}>
+              {status}
+              {lat && lng && <Marker position={[lat, lng]}>
+                <Popup>
+                  <div>
+                    You are here.
+                  </div>
+                </Popup></Marker>}
+            </Button>
+          </div>
+          <BaseLayer checked name="Imagery">
+            <BasemapLayer name='Imagery' center={[42.704180, -73.731980]}
+              zoom={14}></BasemapLayer>
+          </BaseLayer>
+          <BaseLayer name="ImageryClarity">
+            <BasemapLayer name='ImageryClarity' center={[42.704180, -73.731980]}
+              zoom={14}></BasemapLayer>
+          </BaseLayer>
+          <BaseLayer name="NationalGeographic">
+            <BasemapLayer name='NationalGeographic' center={[42.704180, -73.731980]}
+              zoom={14}></BasemapLayer>
+          </BaseLayer>
+          <BaseLayer name='Streets'>
+            <BasemapLayer name='Streets' center={[42.704180, -73.731980]}
+              zoom={14}></BasemapLayer>
+          </BaseLayer>
           <LayerGroup>
             <LayersControl.Overlay name="Roads">
               <GeoJSON data={ARC_Roads}></GeoJSON>
             </LayersControl.Overlay>
           </LayerGroup>
           <LayerGroup>
-            <LayersControl.Overlay name="Boundary">
+            <LayersControl.Overlay checked name="Boundary">
               <GeoJSON data={ARC_Boundary}></GeoJSON>
             </LayersControl.Overlay>
           </LayerGroup>
@@ -181,34 +201,21 @@ export default function Map() {
               </GeoJSON>
             </LayersControl.Overlay>
           </LayerGroup>
-          <div>  
+          <div>
             {displaySubset}
           </div>
-        
-          {/*
-            lat && lng && valueI === 999 && 
-            <Marker position={[lng, lat]}>
-              <Popup>
-                <div>
-                  <h1>
-                    <span role="img" aria-label="burial">
-                      {fullName}
-                    </span>
-                  </h1>
-                </div>
-              </Popup>
-            </Marker>
-              */}
         </LayersControl>
       </MapContainer>
 
-      <div className="ag-theme-alpine" style={{ height: '40vh' }}>
+      <div className="ag-theme-material" style={{ height: '40vh' }}>
         <AgGridReact
+          defaultColDef={defaultColDef}
           ref={gridRef}
           pagination={true}
           paginationAutoPageSize={true}
           rowData={geo_burials_rows}
           columnDefs={columns}
+          columnHoverHighlight={true}
           rowSelection="multiple">
         </AgGridReact>
 
